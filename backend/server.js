42d557ac26d855c5e815db9074dd964faf12131b
RanaJyotika <srrana956@gmail.com>
2024-07-18 11:22:50 +0530

    Socket.io implemented

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -7,26 +7,24 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDb from "./db/connectToMongoDb.js";
+import {app, server} from "./socket/socket.js";
 
-const app = express ();
-const PORT = process.env.PORT || 5000;
 
 dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads (from.req.body)
 app.use(cookieParser());
 
+
 app.use("/api/auth", authRoutes);   //using middleware for routes
 app.use("/api/messages", messageRoutes); 
 app.use("/api/users", userRoutes); 
-  
-// app.get("/", (req,res) => {
-//     // root route http://localhost:5000/
-//     res.send("Hello World!");
-// });
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDb();
     console.log(`Server is running on port ${PORT}`);
 }
